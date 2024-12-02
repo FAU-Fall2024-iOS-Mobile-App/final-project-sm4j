2929ba39ef926671bcdc65ea4e2c2912128f2f93
sm4j <jsmall2018@fau.edu>
2024-12-02 17:12:05 -0500

    Added splash screen and login screen (no login functionality yet)

---
+++ MarvelDreamTeams/MarvelDreamTeams/MarvelDreamTeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelDreamTeamsApp.swift
+//  MarvelDreamTeams
+//
+//  Created by Jordan Small on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelDreamTeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
